5544c639b07bb31615281eb936b0904fe83473bc
TiagoPrado14 <tiagoprado1984@gmail.com>
2023-07-12 16:08:08 -0300

    Removendo cenários de teste

--- tests/pages/tasks/index.ts
+++ tests/pages/tasks/index.ts
@@ -65,47 +65,4 @@ export class TasksPage {
         const target = this.page.locator("//div[@class='mt-2 SignIn_error__1m21R'][contains(.,'Senha inválida.')]")
         await expect(target).toBeVisible()
     }
-
-    // async travelSearch() {
-    //     const inputOriginTo = this.page.locator('#origin-to')
-    //     await inputOriginTo.fill('brasilia')
-    //     await this.page.click('p[title="BRASILIA - DF"]')
-    //     const inputGoingTo = this.page.locator('#going-to')
-    //     await inputGoingTo.fill('sao paulo')
-    //     await this.page.click('p[title="SAO PAULO (TIETÊ) - SP"]')
-    //     await this.page.click('input[placeholder="Selecione Data"]')
-    //     await this.page.click('td[class*="CalendarDay__today"]')
-    //     await this.page.click('//button[text()="Buscar passagem"]')
-    // }
-
-    // async travelSelect() {
-    //     await this.page.click("//button[contains(@id,'0')][@type='button'][contains(.,'Selecione esta viagem')]")
-    // }
-
-    // async selecionarAssento() {
-
-    //     let indexAssento = 0;
-
-    //     // while (true) {
-    //     //     let assento = `#seat_${indexAssento}`;
-    //     //     try {
-    //     //         this.page.setDefaultTimeout(300)
-    //     //         await this.page.waitForSelector(assento, { state: 'visible' });
-    //     //         await this.page.click(assento)
-    //     //         break;
-    //     //     } catch (error) {
-    //     //         indexAssento += 1
-    //     //     }
-    //     // }
-
-    //     await this.page.click('xpath=//button[text()="Escolher"]')
-
-    // }
-
-    // async checkout() {
-    //     await this.page.click('div[class*="InputBase-formControl"]')
-    //     await this.page.click('//li[text()="Teste Payer Primeiro"]')
-    //     await this.page.click('#btnViagem_0')
-    // }
-
 }
