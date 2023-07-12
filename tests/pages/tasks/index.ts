4451acb44404a182592c81abffc35a86e2b1099f
TiagoPrado14 <tiagoprado1984@gmail.com>
2023-07-12 16:06:09 -0300

    Enviando alterações no projeto

--- tests/pages/tasks/index.ts
+++ tests/pages/tasks/index.ts
@@ -16,52 +16,96 @@ export class TasksPage {
     async login() {
         await this.page.click('div[class*="UserMenu_user"]')
         const inputEmail = this.page.locator('#login_email')
-        await inputEmail.fill('test_payer_1685127824@testuser.com')
+        await inputEmail.fill('tiago.silva@gipsyy.com.br')
         const inputPassword = this.page.locator('#password')
-        await inputPassword.fill('Tp@123456')
+        await inputPassword.fill('Nova1010-')
         await this.page.click('button[type=submit]')
+        
     }
 
-    async travelSearch() {
-        const inputOriginTo = this.page.locator('#origin-to')
-        await inputOriginTo.fill('brasilia')
-        await this.page.click('p[title="BRASILIA - DF"]')
-        const inputGoingTo = this.page.locator('#going-to')
-        await inputGoingTo.fill('sao paulo')
-        await this.page.click('p[title="SAO PAULO (TIETÊ) - SP"]')
-        await this.page.click('input[placeholder="Selecione Data"]')
-        await this.page.click('td[class*="CalendarDay__today"]')
-        await this.page.click('//button[text()="Buscar passagem"]')
+    async logout() {
+        await this.page.click('div[class*="UserMenu_user"]')
+        await this.page.click("//span[@class='UserMenu_text__SE8CN'][contains(.,'Sair')]")
+        const target = this.page.locator('div[class*="UserMenu_user"]')
+        await expect(target).toBeVisible()
     }
 
-    async travelSelect() {
-        await this.page.click("//button[contains(@id,'0')][@type='button'][contains(.,'Selecione esta viagem')]")
+    async validarLogin() {
+        const target = this.page.locator('div[class*="loggedUser"]')
+        await expect(target).toBeVisible()
     }
 
-    async selecionarAssento() {
-
-        let indexAssento = 0;
-
-        // while (true) {
-        //     let assento = `#seat_${indexAssento}`;
-        //     try {
-        //         this.page.setDefaultTimeout(300)
-        //         await this.page.waitForSelector(assento, { state: 'visible' });
-        //         await this.page.click(assento)
-        //         break;
-        //     } catch (error) {
-        //         indexAssento += 1
-        //     }
-        // }
-
-        await this.page.click('xpath=//button[text()="Escolher"]')
-
+    async loginInvalido () {
+        await this.page.click('div[class*="UserMenu_user"]')
+        const inputEmail = this.page.locator('#login_email')
+        await inputEmail.fill('jose@teste.com.br')
+        const inputPassword = this.page.locator('#password')
+        await inputPassword.fill('Nova1010-')
+        await this.page.click('button[type=submit]')
+        const target = this.page.locator("//div[@class='mt-2 SignIn_error__1m21R'][contains(.,'Email não encontrado.')]")
+        await expect(target).toBeVisible()
     }
 
-    async checkout() {
-        await this.page.click('div[class*="InputBase-formControl"]')
-        await this.page.click('//li[text()="Teste Payer Primeiro"]')
-        await this.page.click('#btnViagem_0')
+    async msgEmailInvalido() {
+        await this.page.click('div[class*="UserMenu_user"]')
+        const inputEmail = this.page.locator('#login_email')
+        await inputEmail.fill('andregipsyy.com.br')
+        await this.page.click('#password')
+        const target = this.page.locator("//div[@class='SignIn_error__1m21R'][contains(.,'Por favor digite um E-Mail válido (e.g. name@email.com)')]")
+        await expect(target).toBeVisible()
     }
+
+    async msgSenhaInvalida() {
+        await this.page.click('div[class*="UserMenu_user"]')
+        const inputEmail = this.page.locator('#login_email')
+        await inputEmail.fill('tiago.silva@gipsyy.com.br')
+        const inputPassword = this.page.locator('#password')
+        await inputPassword.fill('Nova1010')
+        await this.page.click('button[type=submit]')
+        const target = this.page.locator("//div[@class='mt-2 SignIn_error__1m21R'][contains(.,'Senha inválida.')]")
+        await expect(target).toBeVisible()
+    }
+
+    // async travelSearch() {
+    //     const inputOriginTo = this.page.locator('#origin-to')
+    //     await inputOriginTo.fill('brasilia')
+    //     await this.page.click('p[title="BRASILIA - DF"]')
+    //     const inputGoingTo = this.page.locator('#going-to')
+    //     await inputGoingTo.fill('sao paulo')
+    //     await this.page.click('p[title="SAO PAULO (TIETÊ) - SP"]')
+    //     await this.page.click('input[placeholder="Selecione Data"]')
+    //     await this.page.click('td[class*="CalendarDay__today"]')
+    //     await this.page.click('//button[text()="Buscar passagem"]')
+    // }
+
+    // async travelSelect() {
+    //     await this.page.click("//button[contains(@id,'0')][@type='button'][contains(.,'Selecione esta viagem')]")
+    // }
+
+    // async selecionarAssento() {
+
+    //     let indexAssento = 0;
+
+    //     // while (true) {
+    //     //     let assento = `#seat_${indexAssento}`;
+    //     //     try {
+    //     //         this.page.setDefaultTimeout(300)
+    //     //         await this.page.waitForSelector(assento, { state: 'visible' });
+    //     //         await this.page.click(assento)
+    //     //         break;
+    //     //     } catch (error) {
+    //     //         indexAssento += 1
+    //     //     }
+    //     // }
+
+    //     await this.page.click('xpath=//button[text()="Escolher"]')
+
+    // }
+
+    // async checkout() {
+    //     await this.page.click('div[class*="InputBase-formControl"]')
+    //     await this.page.click('//li[text()="Teste Payer Primeiro"]')
+    //     await this.page.click('#btnViagem_0')
+    // }
 
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 import { TasksPage } from './pages/tasks'
 
 let tasksPage: TasksPage
@@ -7,14 +7,32 @@ test.beforeEach(({ page }) => {
     tasksPage = new TasksPage(page)
 })
 
-test.describe('Comprar bilhete', () => {
-    test('Comprar bilhete com pagamento pix', async () => {
+test.describe('Login', () => {
+    test('Realizar um login com informações válidas', async () => {
         await tasksPage.go()
         await tasksPage.login()
-        await tasksPage.travelSearch()
-        await tasksPage.travelSelect() 
-        await tasksPage.selecionarAssento()
-        await tasksPage.checkout()         
+        await tasksPage.validarLogin()
+    })
+
+    test('Realizar logout do usuário', async () => {
+        await tasksPage.go()
+        await tasksPage.login()
+        await tasksPage.logout()
+    })
+
+    test('Realizar um login com informações inválidas', async () => {
+        await tasksPage.go()
+        await tasksPage.loginInvalido()
+    })
+
+    test('Validar mensagem ao iserir um email inválido', async () => {
+        await tasksPage.go()
+        await tasksPage.msgEmailInvalido()
+    })
+
+    test('Validar mensagem ao inserir uma senha invalida', async () => {
+        await tasksPage.go()
+        await tasksPage.msgSenhaInvalida()
     })
 })
 
